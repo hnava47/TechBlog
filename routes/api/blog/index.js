697f2118690b29d851df649b740d9a227dfc26cf
Hector Nava <hector.r.nava44@gmail.com>
2022-03-31 20:58:40 -0600

    Added blog routes

--- routes/api/blog/index.js
+++ routes/api/blog/index.js
@@ -1,16 +1,31 @@
 const router = require('express').Router();
+const {
+    viewAllBlogs,
+    viewBlog,
+    createBlog,
+    updateBlog,
+    deleteBlog
+} = require('../../../controllers/blogController');
+const {
+    createComment,
+    updateComment,
+    deleteComment
+} = require('../../../controllers/commentController');
 
 router.route('/')
-    .get()
-    .post();
+    .get(viewAllBlogs)
+    .post(createBlog);
 
 router.route('/:blogId')
-    .get()
-    .patch()
-    .post()
-    .delete();
+    .get(viewBlog)
+    .patch(updateBlog)
+    .delete(deleteBlog);
 
 router.route('/:blog/comment')
-    .post();
+    .post(createComment);
+
+router.route('/:blog/comment/:commentId')
+    .patch(updateComment)
+    .delete(deleteComment);
 
 module.exports = router;
